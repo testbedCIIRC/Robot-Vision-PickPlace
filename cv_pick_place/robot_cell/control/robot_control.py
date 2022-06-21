ac658de56c57325446f261681a3c5f85aadfbcc8
David S. Martinez <56695799+davidmartinez13@users.noreply.github.com>
2022-06-21 19:13:02 +0200

    added robot communication

--- cv_pick_place/robot_cell/control/robot_control.py
+++ cv_pick_place/robot_cell/control/robot_control.py
@@ -15,8 +15,9 @@ from scipy import ndimage
 from queue import Queue
 from threading import Thread
 from collections import OrderedDict
+from robot_cell.control.robot_communication import RobotCommunication
 
-class RobotControl:
+class RobotControl(RobotCommunication):
     def __init__(self, rob_dict):
         """
         RobotControl object constructor.
@@ -25,172 +26,8 @@ class RobotControl:
         rob_dict (dict): Dictionary with robot points for program. 
 
         """
-        self.rob_dict = rob_dict
+        super().__init__(rob_dict)
 
-    def connect_OPCUA_server(self):
-        """
-        Connects OPC UA Client to Server on PLC.
-
-        """
-        password = "CIIRC"
-        self.client = Client("opc.tcp://user:"+str(password)+"@10.35.91.101:4840/")
-        self.client.connect()
-        print('[INFO]: Client connected.')
-
-    def get_nodes(self):
-        """
-        Using the client.get_node method, it gets nodes from OPCUA Server on PLC.
-
-        """
-        self.Start_Prog = self.client.get_node(
-            'ns=3;s="HMIKuka"."robot"."example"."pickPlace"."command"."start"')
-        self.Conti_Prog = self.client.get_node(
-            'ns=3;s="HMIKuka"."robot"."example"."pickPlace"."command"."continue"')
-        self.Stop_Prog = self.client.get_node(
-            'ns=3;s="HMIKuka"."robot"."example"."pickPlace"."command"."interrupt"')
-        self.Abort_Prog = self.client.get_node(
-            'ns=3;s="HMIKuka"."robot"."powerRobot"."command"."abort"')
-        self.Prog_Done = self.client.get_node(
-            'ns=3;s="HMIKuka"."robot"."example"."pickPlace"."status"."done"')
-        self.Stop_Active = self.client.get_node(
-            'ns=3;s="InstPickPlace"."instInterrupt"."BrakeActive"')
-        self.Rob_Stopped = self.client.get_node(
-            'ns=3;s="InstKukaControl"."instAutomaticExternal"."ROB_STOPPED"')
-        self.Conveyor_Left = self.client.get_node(
-            'ns=3;s="conveyor_left"')
-        self.Conveyor_Right = self.client.get_node(
-            'ns=3;s="conveyor_right"')
-        self.Gripper_State = self.client.get_node(
-            'ns=3;s="gripper_control"')
-        self.Encoder_Vel = self.client.get_node(
-            'ns=3;s="Encoder_1".ActualVelocity')
-        self.Encoder_Pos = self.client.get_node(
-            'ns=3;s="Encoder_1".ActualPosition')
-        self.Laser_Enable = self.client.get_node(
-            'ns=3;s="laser_field_enable"')
-
-        self.Act_Pos_X = self.client.get_node(
-            'ns=3;s="InstKukaControl"."instReadActualPos"."X"')
-        self.Act_Pos_Y = self.client.get_node(
-            'ns=3;s="InstKukaControl"."instReadActualPos"."Y"')
-        self.Act_Pos_Z = self.client.get_node(
-            'ns=3;s="InstKukaControl"."instReadActualPos"."Z"')
-        self.Act_Pos_A = self.client.get_node(
-            'ns=3;s="InstKukaControl"."instReadActualPos"."A"')
-        self.Act_Pos_B = self.client.get_node(
-            'ns=3;s="InstKukaControl"."instReadActualPos"."B"')
-        self.Act_Pos_C = self.client.get_node(
-            'ns=3;s="InstKukaControl"."instReadActualPos"."C"')
-        self.Act_Pos_Turn = self.client.get_node(
-            'ns=3;s="InstKukaControl"."instReadActualPos"."Turn"')
-        self.Act_Pos_Status = self.client.get_node(
-            'ns=3;s="InstKukaControl"."instReadActualPos"."Status"')
-
-        self.Home_X = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[0]."E6POS"."X"')
-        self.Home_Y = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[0]."E6POS"."Y"')
-        self.Home_Z = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[0]."E6POS"."Z"')
-        self.Home_A = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[0]."E6POS"."A"')
-        self.Home_B = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[0]."E6POS"."B"')
-        self.Home_C = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[0]."E6POS"."C"')
-        self.Home_Status = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[0]."E6POS"."Status"')
-        self.Home_Turn = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[0]."E6POS"."Turn"')
-
-        self.PrePick_Pos_X = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[1]."E6POS"."X"')
-        self.PrePick_Pos_Y = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[1]."E6POS"."Y"')
-        self.PrePick_Pos_Z = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[1]."E6POS"."Z"')
-        self.PrePick_Pos_A = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[1]."E6POS"."A"')
-        self.PrePick_Pos_B = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[1]."E6POS"."B"')
-        self.PrePick_Pos_C = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[1]."E6POS"."C"')
-        self.PrePick_Pos_Status = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[1]."E6POS"."Status"')
-        self.PrePick_Pos_Turn = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[1]."E6POS"."Turn"')
-
-        self.Pick_Pos_X = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[2]."E6POS"."X"')
-        self.Pick_Pos_Y = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[2]."E6POS"."Y"')
-        self.Pick_Pos_Z = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[2]."E6POS"."Z"')
-        self.Pick_Pos_A = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[2]."E6POS"."A"')
-        self.Pick_Pos_B = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[2]."E6POS"."B"')
-        self.Pick_Pos_C = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[2]."E6POS"."C"')
-        self.Pick_Pos_Status = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[2]."E6POS"."Status"')
-        self.Pick_Pos_Turn = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[2]."E6POS"."Turn"')
-
-        self.Place_Pos_X = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[3]."E6POS"."X"')
-        self.Place_Pos_Y = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[3]."E6POS"."Y"')
-        self.Place_Pos_Z = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[3]."E6POS"."Z"')
-        self.Place_Pos_A = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[3]."E6POS"."A"')
-        self.Place_Pos_B = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[3]."E6POS"."B"')
-        self.Place_Pos_C = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[3]."E6POS"."C"')
-        self.Place_Pos_Status = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[3]."E6POS"."Status"')
-        self.Place_Pos_Turn = self.client.get_node(
-            'ns=3;s="InstPickPlace"."positions"[3]."E6POS"."Turn"')
-
-        self.PrePick_Done =  self.client.get_node(
-            'ns=3;s="InstPickPlace"."instPrePickPos"."Done"')
-        self.Place_Done =  self.client.get_node(
-            'ns=3;s="InstPickPlace"."instPlacePos"."Done"')
-
-    def robot_server(self, server_out):
-        """
-        Thread to get values from PLC server.
-
-        Parameters:
-        server_out (object): Queue object where data from PLC server is placed.
-
-        """
-        # Connect server and get nodes.
-        self.connect_OPCUA_server()
-        self.get_nodes()
-        #Enabling laser sensor to synchornize robot with moving packets.
-        self.Laser_Enable.set_value(ua.DataValue(True))
-        time.sleep(0.5)
-        while True:
-            try:
-                robot_server_dict = {
-                'pos':self.get_actual_pos(),
-                'encoder_vel':round(self.Encoder_Vel.get_value(),2),
-                'encoder_pos':round(self.Encoder_Pos.get_value(),2),
-                'start':self.Start_Prog.get_value(),
-                'abort':self.Abort_Prog.get_value(),
-                'rob_stopped':self.Rob_Stopped.get_value(),
-                'stop_active':self.Stop_Active.get_value(),
-                'prog_done':self.Prog_Done.get_value()
-                }
-                server_out.put(robot_server_dict)
-
-            except:
-                # Triggered when OPCUA server was disconnected.
-                print('[INFO]: Queue empty.')
-                break
 
     def show_boot_screen(self, message):
         """
