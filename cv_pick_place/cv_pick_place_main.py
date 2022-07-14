0ea8254defe46feaa865bec8e51dd8973078185f
Lars Kahlert <kahlert@email.cz>
2022-07-14 14:04:43 +0200

    Changed constants and removed x pick offset

--- cv_pick_place/cv_pick_place_main.py
+++ cv_pick_place/cv_pick_place_main.py
@@ -82,7 +82,7 @@ def main(rob_dict, paths, files, check_point, info_dict, encoder_pos_m, control_
     homography = None # Homography matrix
     state = "READY" # Robot state variable
 
-    grip_time_offset = 1000
+    grip_time_offset = 900
 
     while True:
         # Start timer for FPS estimation
@@ -219,7 +219,7 @@ def main(rob_dict, paths, files, check_point, info_dict, encoder_pos_m, control_
             print("DEBUG: Pick distances")
             print(pick_list_positions)
             # If item is too far remove it from list
-            is_valid_position = pick_list_positions < 1600 - grip_time_offset  # TODO find position after which it does not pick up - depends on enc_vel and robot speed
+            is_valid_position = pick_list_positions < 1700 - grip_time_offset  # TODO find position after which it does not pick up - depends on enc_vel and robot speed
             pick_list = np.ndarray.tolist(np.asanyarray(pick_list)[is_valid_position])     
             pick_list_positions = pick_list_positions[is_valid_position]
             # Choose a item for picking
@@ -251,8 +251,8 @@ def main(rob_dict, paths, files, check_point, info_dict, encoder_pos_m, control_
                 if pick_pos_x < 600.0:
                     pick_pos_x = 600.0
 
-                elif pick_pos_x > 1800.0:
-                    pick_pos_x = 1800.0
+                elif pick_pos_x > 1700.0:
+                    pick_pos_x = 1700.0
 
                 prepick_xyz_coords = np.array([pick_pos_x, pick_pos_y, rob_dict['pick_pos_base'][0]['z']])
 
@@ -262,7 +262,8 @@ def main(rob_dict, paths, files, check_point, info_dict, encoder_pos_m, control_
                     'y': pick_pos_y,
                     'rot': gripper_rot,
                     'packet_type': packet_type,
-                    'x_offset': pack_x_offsets[packet_type],
+                    # 'x_offset': pack_x_offsets[packet_type],
+                    'x_offset': 0,
                     'pack_z': packet_z
                     }
                 control_pipe.send(RcData(RcCommand.CHANGE_TRAJECTORY, trajectory_dict))
@@ -291,7 +292,7 @@ def main(rob_dict, paths, files, check_point, info_dict, encoder_pos_m, control_
             print("X distance")
             print(pick_pos_x - packet_pos_x)
             # If packet is close enough continue picking operation
-            if packet_pos_x > pick_pos_x - 200:
+            if packet_pos_x > pick_pos_x - 280:
                 control_pipe.send(RcData(RcCommand.CONTINUE_PROGRAM))
                 state = "PICKING"
                 print("state: PICKING")
