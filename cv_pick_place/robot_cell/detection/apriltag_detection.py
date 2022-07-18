fb4486d938d5eeb36f946a2827eee703e9e44610
Jiří Štefan <stefaji3@fel.cvut.cz>
2022-07-18 16:33:18 +0200

    Reworked apriltag detection

--- cv_pick_place/robot_cell/detection/apriltag_detection.py
+++ cv_pick_place/robot_cell/detection/apriltag_detection.py
@@ -1,8 +1,9 @@
 import json
 import cv2 
 import numpy as np
+
+
 class ProcessingApriltag:
-    
     def __init__(self):
         """
         ProcessingApriltag object constructor. Initializes data containers.
@@ -14,78 +15,104 @@ class ProcessingApriltag:
         self.world_points_detect = []
         self.image_points_detect = []
         self.homography = None
+        self.tag_corner_list = None
+        self.tag_id_list = None
     
-    def load_original_points(self):
+    def load_world_points(self, file_path):
         """
-        Loads conveyor world points from the json file.
+        Loads conveyor world points from a json file.
+
+        Parameters:
+        file_path (string): Path to a json file containing coordinates
         
         """
-        f = open('conveyor_points.json')
-        # Dict of points in conveyor:
-        self.world_points = json.load(f) 
+        with open(file_path, 'r') as f:
+            self.world_points = json.load(f) 
 
     def compute_homog(self):
         """
         Computes homography matrix using image and conveyor world points.
 
         Returns:
-        numpy.ndarray: Homography matrix as numpy array.
+        numpy.ndarray: Homography matrix as numpy array
         
         """
         for tag_id in self.image_points:
             if tag_id in self.world_points:
                 self.world_points_detect.append(self.world_points[tag_id])
                 self.image_points_detect.append(self.image_points[tag_id])
-        is_enough_points_detect = len(self.image_points_detect)>= 4
-        if is_enough_points_detect:
-            self.homography,status = cv2.findHomography(np.array(self.image_points_detect), 
-                                                np.array(self.world_points_detect))
-            return self.homography
-        else:
-            print("[INFO]: Less than 4 corresponding points found")
-            return self.homography
 
-    def detect_tags(self, color_image, image_frame = None):
+        # Only update homography matrix if enough points were detected
+        is_enough_points_detect = len(self.image_points_detect) >= 4
+        
+        if is_enough_points_detect:
+            self.homography, _ = cv2.findHomography(np.array(self.image_points_detect), 
+                                                         np.array(self.world_points_detect))
+            print('[INFO]: Homography matrix updated')
+        else:
+            print("[INFO]: Less than 4 AprilTags found in frame, new homography matrix was not computed")
+        
+        return self.homography
+
+    def detect_tags(self, color_frame):
         """
-        Detects and draws tags on a copy if the input image.
+        Detects april tags in the input image.
         
         Parameters:
-        color_image (numpy.ndarray): Input image where apriltags are to be detected.
-
-        Returns:
-        numpy.ndarray: Image with apriltags.
+        color_image (numpy.ndarray): Image where apriltags are to be detected
         
         """
-        rect1 = []
-
-        if image_frame is None or not image_frame.shape == color_image.shape:
-            image = color_image.copy()
-        else:
-            image = image_frame
-
-        self.load_original_points()
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        (corners, ids, rejected) = cv2.aruco.detectMarkers( image, cv2.aruco.Dictionary_get(
-                                    cv2.aruco.DICT_APRILTAG_36h11),
-                                    parameters=cv2.aruco.DetectorParameters_create())
-        ids = ids.flatten()
-        int_corners = np.int0(corners)
-        cv2.polylines(image, int_corners, True, (0, 255, 0), 2)
+        gray_frame = cv2.cvtColor(color_frame, cv2.COLOR_BGR2GRAY)
+        (corners, ids, rejected) = cv2.aruco.detectMarkers(gray_frame,
+                                                           cv2.aruco.Dictionary_get(cv2.aruco.DICT_APRILTAG_36h11),
+                                                           parameters=cv2.aruco.DetectorParameters_create())
         
-        for (tag_corner, tag_id) in zip(corners, ids):
-            # get (x,y) corners of tag
-            corners = tag_corner.reshape((4, 2))
+        # If nothing was detected, return
+        if len(corners) == 0 or ids is None:
+            return
+
+        self.tag_corner_list = corners
+        self.tag_id_list = ids.flatten()
+        
+        for (tag_corners, tag_id) in zip(self.tag_corner_list, self.tag_id_list):
+            # Get (x, y) corners of the tag
+            corners = tag_corners.reshape((4, 2))
             (top_left, top_right, bottom_right, bottom_left) = corners
-            top_right, bottom_right = (int(top_right[0]), int(top_right[1])),\
-                                      (int(bottom_right[0]), int(bottom_right[1]))
-            bottom_left, top_left = (int(bottom_left[0]), int(bottom_left[1])),\
-                                    (int(top_left[0]), int(top_left[1]))
-            # compute centroid
+
+            top_left = (int(top_left[0]), int(top_left[1]))
+            top_right = (int(top_right[0]), int(top_right[1]))
+            bottom_right = (int(bottom_right[0]), int(bottom_right[1]))
+            bottom_left = (int(bottom_left[0]), int(bottom_left[1]))
+
+            # Compute centroid
             cX = int((top_left[0] + bottom_right[0]) / 2.0)
             cY = int((top_left[1] + bottom_right[1]) / 2.0)
-            self.image_points[str(int(tag_id))] = [cX,cY]
-            cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
-            # draw ID on frame
-            cv2.putText(image, str(tag_id),(top_left[0], top_left[1] - 15),
-                        cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 255, 0), 2)
-        return image
+
+            # Store detected points for homography computation
+            self.image_points[str(int(tag_id))] = [cX, cY]
+
+    def draw_tags(self, image_frame):
+        """
+        Draws detected april tags into image frame.
+        
+        Parameters:
+        image_frame (numpy.ndarray): Image where apriltags are to be drawn
+
+        Returns:
+        numpy.ndarray: Image with drawn april tags
+
+        """
+        if not isinstance(image_frame, np.ndarray):
+            print("[WARNING] Tried to draw AprilTags into something which is not numpy.ndarray image frame")
+            return image_frame
+
+        if self.tag_corner_list is None or self.tag_id_list is None:
+            return image_frame
+        
+        cv2.polylines(image_frame, np.int0(self.tag_corner_list), True, (0, 255, 0), 2)
+        
+        for tag_id in self.tag_id_list:
+            text = str(int(tag_id))
+            cv2.putText(image_frame, text, (self.image_points[str(int(tag_id))][0] + 30, self.image_points[str(int(tag_id))][1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        
+        return image_frame
